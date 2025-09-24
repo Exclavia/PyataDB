f8e31e1a8bec100168fe3259a960c07f0dcb0159
Exclavia <git@exclavia.network>
2025-09-24 07:10:37 -0400

    Update __init__.py

--- PyataDB/__init__.py
+++ PyataDB/__init__.py
@@ -1,1 +1,14 @@
+"""
+PyataDB is a lightweight, easy-to-use, and efficient database built entirely in Python.
+
+https://github.com/Exclavia/PyataDB
+"""
+# std
+import sys
+import platform
+
+# pyata module
 from .Pyata import *
+
+__all__ = ('Pydb', 'PyTable',)
+__version__ = "0.0.3"
