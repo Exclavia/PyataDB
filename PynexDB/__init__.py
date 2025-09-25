cc41754b9484b9bb9b2fefbc512aa31a7528c8d8
Exclavia <git@exclavia.network>
2025-09-25 12:23:54 -0400

    Update __init__.py

--- PynexDB/__init__.py
+++ PynexDB/__init__.py
@@ -6,8 +6,6 @@ https://github.com/Exclavia/PynexDB
 # std
 import os as __os__
 import sys as __sys__
-from collections import defaultdict as __defdict__
-import pickle as __pickle__
 
 # pynex module
 from .pynex import *
