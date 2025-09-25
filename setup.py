4253f7d3d25ab017366a1179ec6bd619df9754e1
Exclavia <git@exclavia.network>
2025-09-25 11:17:55 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='PynexDB',
+    version='0.1.4',
+    author='Exclavia',
+    description='PynexDB is a lightweight, easy-to-use, and efficient database built entirely in Python',
+    packages=find_packages(),
+)
