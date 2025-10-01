9199425a6251a3f7a18cf63d3f733cc53e69f1d9
Exclavia <git@exclavia.network>
2025-10-01 16:37:47 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name='PynexDB',
-    version='0.1.6',
+    version='2.0.1',
     author='Exclavia',
-    description='PynexDB is a lightweight, easy-to-use, and efficient database built entirely in Python',
+    description='PynexDB is a lightweight, efficient, and easy-to-use, and efficient database built entirely in Python',
     packages=find_packages(),
 )
+
