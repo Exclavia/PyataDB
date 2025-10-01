0fa950a848b1ad0c8d742ce73e2e13498b06de3c
Exclavia <git@exclavia.network>
2025-10-01 17:17:09 -0400

    Update __init__.py

--- PynexDB/__init__.py
+++ PynexDB/__init__.py
@@ -1,16 +1,15 @@
 """
-A lightweight and easy-to-use database written in Python
+A lightweight, efficient, and easy-to-use database written in Python
 
 https://github.com/Exclavia/PynexDB
 """
 # std
-import os as __os__
-import sys as __sys__
+import os
+import sys
 
 # pynex module
-from .Pynex import (PyDb, PyTb,)
-from .Tools import (ItemCount, ToJSON,)
+import Pynex
 
 
-__all__ = ['PyDb', 'PyTb', 'ItemCount', 'ToJSON']
-__version__ = "0.1.6"
+__all__ = ['Database', 'Table', 'Transaction', 'OPERATORS']
+__version__ = "2.0.1"
